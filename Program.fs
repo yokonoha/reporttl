3cd0b29448e33e8600bde4d69be96b9db65b768b
yokonoha <askjp.team@gmail.com>
2024-11-24 19:42:58 +0900

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,7 @@
+﻿[<EntryPoint>]
+let main argv =
+    let len=argv.[0].Length
+    if argv.Length=1 then printfn "[Report text length]The length of the text is %d" len
+    else printfn "Error: Please enclose your text in \"\". Or do not send multiple arguments."
+    0
+    //ただ単にF#に触りたかったので製作しました!(by横葉)
